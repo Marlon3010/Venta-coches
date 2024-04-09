4ae32db62feb23f3fa14ae90eb3182e1556d3c6b
Daniel Moya <me@example.com>
2024-04-08 23:41:25 -0500

    agregar header en archivo app y comentarios en archivo readme

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # Venta-coches
 
+# Repositorio
+
 # https://github.com/Marlon3010/Venta-coches
 
+# aplicación web
+
 # https://venta-coches.onrender.com/
+
+# La app se encuentra conformada por:
+
+# Un encabazado que describe la app
+
+# Contiene un check box que realiza la acción de mostrar/no mostrar el histograma
+
+# Contiene un check box que realiza la acción de mostrar/no mostrar diagrama de dispersión
--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import streamlit as st
 
 car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
 
+st.header('Gráfica de histograma y diagrama de dispersión')
 # hist_button = st.button('Construir histograma')  # crear un botón
 build_histogram = st.checkbox('Construir un histograma')
 
@@ -20,13 +21,13 @@ if build_histogram:  # al hacer clic en el botón
     st.plotly_chart(fig1, use_container_width=True)
 
 # hist_button2 = st.button('Construir dispersión')  # crear un botón
-build_histogram2 = st.checkbox('Construir un histograma dispersión')
+build_histogram2 = st.checkbox('Construir un diagrama de dispersión')
 
 if build_histogram2:
     # hist_button2:  # al hacer clic en el botón
     # escribir un mensaje
     st.write(
-        'Creación de un histograma dispersion')
+        'Creación de un diagrama de dispersion para el conjunto de datos de anuncios de venta de coches')
 
     # crear un gráfico de dispersión
     fig2 = px.scatter(car_data, x="odometer", y="price")
